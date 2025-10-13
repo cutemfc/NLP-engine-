77f70d63981e579b1d3eb5aba4a0cf7433e362e6
Mei-Fang Chen <132273645+cutemfc@users.noreply.github.com>
2025-10-13 12:29:01 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -4,14 +4,16 @@ import streamlit as st
 import csv
 from pathlib import Path
 from datetime import datetime
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
 
 st.title("Rental Request Form")
 
 # User input fields
-name = st.text_input("Your name")
-email = st.text_input("Your email")
-request = st.text_input("Request about House or Apartment renting:")
+name = st.text_input("Your name",key="name_input")
+email = st.text_input("Your email",key="email_input")
+request = st.text_input("Request about House or Apartment renting:", key="request_input")
 
 # Submit button
 if st.button("Submit"):
@@ -29,3 +31,23 @@ if st.button("Submit"):
             if not file_exists:
                 writer.writerow(["timestamp", "name", "email", "request"])  # header
             writer.writerow([datetime.now().isoformat(), name, email, request])
+        # Save  to Google Sheets
+        try:
+            scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+            creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
+            client = gspread.authorize(creds)
+            sheet = client.open("House Requests").sheet1  # Open the Google Sheet
+            sheet.append_row([datetime.now().isoformat(), name, email, request])  #
+            st.info("Data saved to Google Sheets.")
+        except Exception as e:
+            st.error(f"Failed to save to Google Sheets: {e}")
+        
+
+
+
+        
+       
+    
+        
+
+
